2886a5ec79feb2f8d0e082b9c5ec011aaee3e09a
Mxdden01 <125059932+Mxdden01@users.noreply.github.com>
2024-12-07 13:49:41 +0000

    Add project files.

---
+++ vMenuServer/FiveMHttpRequest.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CitizenFX.Core.Native;
+using CitizenFX.Core;
+using Newtonsoft.Json;
+using DiscordWebhook;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+
+namespace DiscordLogs
+{
+    public class FiveMHttpRequests : BaseScript
+    {
+        static readonly Dictionary<int, PendingRequest> _pendingRequests = new Dictionary<int, PendingRequest>();
+        public FiveMHttpRequests()
+        {
+            EventHandlers["__cfx_internal:httpResponse"] += new Action<int, int, string, dynamic>(OnHttpResponse);
+        }
+
+        private void OnHttpResponse(int token, int statusCode, string body, dynamic headers)
+        {
+            if (_pendingRequests.TryGetValue(token, out var req))
+            {
+                if (statusCode == 200)
+                {
+                    req.SetResult(body);
+                }
+                else
+                {
+                    req.SetException(new Exception("Server returned status code: " + statusCode));
+                }
+
+                _pendingRequests.Remove(token);
+            }
+        }
+
+        public static async Task<string> DownloadString(string url)
+        {
+            var args = new Dictionary<string, object>() {
+            { "url", url }
+        };
+            var argsJson = JsonConvert.SerializeObject(args);
+            var id = API.PerformHttpRequestInternal(argsJson, argsJson.Length);
+            var req = _pendingRequests[id] = new PendingRequest(id);
+            return await req.Task;
+        }
+        public static async Task<string> UploadString(string url, string body)
+        {
+            var args = new Dictionary<string, object>() {
+            { "url", url },
+            { "method", "POST" },
+            { "data", body },
+            { "headers", new Dictionary<string, string> { { "Content-Type", "application/json" } } }
+        };
+            var argsJson = JsonConvert.SerializeObject(args);
+            var id = API.PerformHttpRequestInternal(argsJson, argsJson.Length);
+            var req = _pendingRequests[id] = new PendingRequest(id);
+            return await req.Task;
+        }
+
+        private class PendingRequest : TaskCompletionSource<string>
+        {
+            public int Token;
+            public PendingRequest(int token)
+            {
+                Token = token;
+            }
+        }
+
+        // Discord stuff.
+        public static dynamic DiscordEmbed(string title, string field, string footerText)
+        {
+            return new
+            {
+                title = title,
+                description = field,
+                color = "1127128",
+                footer = new
+                {
+                    text = footerText
+                },
+                thumbnail = new
+                {
+                    url = "https://i.imgur.com/kVJP22U.png"
+                }
+            };
+        }
+
+        public static async Task SendWebhook(string webhook, string content, object embed)
+        {
+            string getWebhook = WebhookConfig.config[webhook];
+            try
+            {
+                await UploadString(getWebhook,
+                    JsonConvert.SerializeObject(new
+                    {
+                        username = "Project Urban vMenu Logs",
+                        content = content,
+                        embeds = new[]
+                        {
+                            embed
+                        }
+                    }
+                    ));
+            }
+            catch { }
+        }
+
+        public static async Task SendWebhook(string webhook, string content, object[] embeds)
+        {
+            string getWebhook = WebhookConfig.config[webhook];
+            try
+            {
+                await UploadString(getWebhook,
+                    JsonConvert.SerializeObject(new
+                    {
+                        content = content,
+                        embeds = embeds
+                    }
+                    ));
+            }
+            catch { }
+        }
+    }
+}
---
+++ vMenuServer/WebhookConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DiscordWebhook
+{
+    public class WebhookConfig
+    {
+        public static IDictionary<string, string> config = new Dictionary<string, string>()
+        {
+            {"vehicle", "https://discord.com/api/webhooks/1314729647178649702/JuPB1xHevPsnpz8ehgNju_ROuEte55wN59w2Mx2Yg0YOZOODEmuZlPMavGSasXMXccCc" },
+            {"vehOptions", "https://discord.com/api/webhooks/1314729647178649702/JuPB1xHevPsnpz8ehgNju_ROuEte55wN59w2Mx2Yg0YOZOODEmuZlPMavGSasXMXccCc" },
+            {"staff", "https://discord.com/api/webhooks/1314729647178649702/JuPB1xHevPsnpz8ehgNju_ROuEte55wN59w2Mx2Yg0YOZOODEmuZlPMavGSasXMXccCc" },
+            {"pOptions", "https://discord.com/api/webhooks/1314729647178649702/JuPB1xHevPsnpz8ehgNju_ROuEte55wN59w2Mx2Yg0YOZOODEmuZlPMavGSasXMXccCc" },
+            {"wOptions", "https://discord.com/api/webhooks/1314729647178649702/JuPB1xHevPsnpz8ehgNju_ROuEte55wN59w2Mx2Yg0YOZOODEmuZlPMavGSasXMXccCc" }
+        };
+    }
+}
